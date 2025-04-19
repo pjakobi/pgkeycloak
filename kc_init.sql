90eda4eed42ca03e107efa70e50ea27129965fb5
Pascal Jakobi <pascal.jakobi@gmail.com>
2025-04-19 10:51:15 +0200

    Quay.io

--- kc_init.sql
+++ kc_init.sql
@@ -1,3 +1,3 @@
-create user keycloak_user with encrypted password 'thales';
+create user keycloak_user with encrypted password 'p@sswd';
 create database keycloak_db owner keycloak_user;
 grant all privileges on database keycloak_db to keycloak_user;
